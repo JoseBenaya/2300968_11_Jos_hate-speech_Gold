5ab6cc3af1ec07dc7234f40c4fb8adca74e4d5ef
Jose Alfred Benaya <josebenaya@gmail.com>
2023-07-31 22:33:16 +0700

    add gold challenge project

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+from fastapi.responses import JSONResponse
+import streamlit as st
+import requests
+
+app = FastAPI()
+
+@app.get("/")
+async def index():
+    return JSONResponse(
+        content = {
+            "ok": True,
+            "code": 200,
+            "data": {
+                "version": "1.0.0"
+            },
+            "message": "Success"
+        }
+    )
+
+
+
+from routers import cleansing
+from routers import sentiment
+
+app.include_router(cleansing.router, tags=["Cleansing API"])
+app.include_router(sentiment.router, tags=["Sentiment API"])
